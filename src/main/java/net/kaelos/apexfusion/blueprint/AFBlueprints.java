2dc2dc6d80bf3ccb4f879b01faee081588dd73ec
kaelos-dev <kaelos.dev@gmail.com>
2026-07-01 22:14:14 +0300

    added blueprints for multiblock

--- src/main/java/net/kaelos/apexfusion/blueprint/AFBlueprints.java
+++ src/main/java/net/kaelos/apexfusion/blueprint/AFBlueprints.java
@@ -1,4 +1,12 @@
 package net.kaelos.apexfusion.blueprint;
 
-public enum AFBlueprints {
+import net.kaelos.apexfusion.AF;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.List;
+
+public class AFBlueprints {
+    public static final List<ResourceLocation> ALL_BLUEPRINTS = List.of(
+            ResourceLocation.fromNamespaceAndPath(AF.MOD_ID, "coke_oven")
+    );
 }
